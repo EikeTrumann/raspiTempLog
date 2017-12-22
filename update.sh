f774d7812e230021583b1da88200098ee814cc64
Eike Trumann <git@eiketrumann.de>
2017-12-22 15:12:12 +0100

    Added Continous Delivery Scripts

--- update.sh
+++ update.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-touch cron.template
-
-# This are the parts of the crontab entry
-cronbase='''* * * * *''' # Update every minute
 basename=$(dirname $BASH_SOURCE)
 cd $basename
-basename=$(pwd)
-scriptname="update.sh"
-
-# This is the line that should be in the crontab file
-echo "$cronbase $basename/$scriptname" > cron.template
-crontab cron.template
-rm cron.template
-
-# Make the update script executable
-chmod +x update.sh
-chmod +x before-update.sh
-chmod +x after-update.sh
+echo "========== update.sh  ========== " >> update.log
+date >> update.log
+# do not activate in programming environments
+# git reset --HARD
+git fetch >> update.log
+commitahead=$(git rev-list HEAD...origin/master --count)
+if (($commitahead != 0))
+	then
+	./before-update.sh >> update.log
+	git rebase >> update.log
+	./after-update.sh >> update.log
+fi
